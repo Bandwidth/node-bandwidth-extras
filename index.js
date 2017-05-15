33a3c237bc6c5091882fce36af4f125f21e1800a
avbel <belchikovs@gmail.com>
2017-05-15 13:19:11 +0300

    Added index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports.application = require('./lib/application');
+module.exports.endpoint = require('./lib/endpoint');
+module.exports.phoneNumber = require('./lib/phone-number');
+
+module.exports.middlewares = require('./lib/middlewares');
