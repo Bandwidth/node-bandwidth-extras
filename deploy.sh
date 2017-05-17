52b9b2eab602e7d9e8f8926a9457b60c34d33107
avbel <belchikovs@gmail.com>
2017-05-17 13:52:19 +0300

    Removed storing of deploy_key in git

--- deploy.sh
+++ deploy.sh
@@ -54,8 +54,7 @@ git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-echo openssl enc -aes-256-cbc -d -in ../deploy_key.enc -out deploy_key -k $ENCRYPTED_KEY
-openssl enc -aes-256-cbc -d -in ../deploy_key.enc -out deploy_key -k "$ENCRYPTED_KEY"
+echo $DEPLOY_KEY | base64 -d > deploy_key
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
