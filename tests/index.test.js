8f14499ce2977195186cc780aba8e8dbd6102382
avbel <belchikovs@gmail.com>
2017-05-17 10:02:34 +0300

    Initial commit

--- tests/index.test.js
+++
@@ -1,9 +0,0 @@
-const test = require('ava');
-const index = require('../index');
-
-test('index.js should export objects', t => {
-	t.truthy(index.application);
-	t.truthy(index.endpoint);
-	t.truthy(index.middlewares);
-	t.truthy(index.phoneNumber);
-});
